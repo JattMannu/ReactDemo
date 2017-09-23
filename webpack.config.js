0073d34d00893b54018777ca083f1b29d444d16f
JattMannu <manpreet_firewall@yahoo.com.sg>
2017-09-23 21:09:11 +0800

    Copied from react

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
