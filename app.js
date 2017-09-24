e53df026228b2bbfda80b6ebe2485123238d5f81
Manpreet Singh <manpreet.singh@autodesk.com>
2017-09-24 14:07:38 +0800

    FInished project for hackton

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { Route } from 'react-router-dom'
+import data from './dataTest.js'
 
 class App extends React.Component {
     constructor(props) {
@@ -33,26 +34,61 @@ class App extends React.Component {
         };
 
         const rooms = [RoomA, RoomB, RoomC, RoomD];
-
+        console.dir(data);
         return <BrowserRouter>
             <div>
-                <h2>Room Database</h2>   {/* HTML element */}
                 <Rooms rooms={rooms} />
-                <Route path='/room' component={sample} />
+                <Route path='/room' component={BookingPage} />
             </div>
         </BrowserRouter>
     }
 }
-
+var globalThis;
 class Rooms extends React.Component {
     constructor(props) {
         super(props);
         console.log('Created a Rooms');
+
+        this.state = {
+            rooms: []
+        }
+
+        globalThis = this;
     }
 
     componentDidMount() {
-        console.log(`componentDidMount ${arguments}`)
-        console.dir(arguments);
+        let header = new Headers({
+            'Access-Control-Allow-Origin': 'http://127.0.0.1:8080/',
+            'Content-Type': 'text/json'
+        });
+        const p_callROOMS = new Promise(function (resolve, reject) {
+            var data = null;
+
+            var xhr = new XMLHttpRequest();
+            xhr.withCredentials = true;
+
+            xhr.addEventListener("readystatechange", function () {
+                if (this.readyState === 4) {
+                    // console.log(this.responseText);
+                    resolve(this.response);
+                }
+            });
+
+            xhr.open("GET", "http://10.208.11.129:9000/api/rhb/");
+            xhr.setRequestHeader("cache-control", "no-cache");
+            xhr.send(data);
+        }).then(function (data) {
+            let _rooms = JSON.parse(data).data;
+            return _rooms;
+        }).then(function (data) {
+
+            // globalThis.props.rooms = data;
+            console.log(data);
+            globalThis.setState({
+                rooms: data
+            });
+        });
+        // p_callROOMS
     }
 
     render() {
@@ -70,14 +106,14 @@ class Rooms extends React.Component {
         return <table style={styleTable}>
             <tbody>
                 <tr>
-                    <th style={styleHeader}>Room Name</th>
+                    <th style={styleHeader}>HotDesk Scanner</th>
                     {/* <th style={styleHeader}>Location</th>
                     <th style={styleHeader}>Book?</th> */}
                 </tr>
 
                 {
-                    this.props.rooms.map(function (room) {
-                        return <Room key={room.id} room={room} />
+                    this.state.rooms.map(function (room) {
+                        return <Room key={room._id} room={room} />
                     })
                 }
             </tbody>
@@ -126,28 +162,113 @@ class Room extends React.Component {
             fontSize: '24px',
             verticalAlign: 'bottom',
             backgroundColor: this.state.isMouseOver ? '#f5f5f5' : '#ffffff',
+            //color: blue;
             borderBottom: '1px solid #ddd',
             textDecoration: 'none'
         }
-        // /}
-        return <tr id={this.props.room.id} onMouseOver={this.onMouseOver.bind(this)} onMouseLeave={this.onMouseLeave.bind(this)} onClick={this.onClick.bind(this)}>
-                <Link to={`/room/${this.props.room.id}`} style={styleRow} >
-                <td>{this.props.room.name}</td>
-                <td>{this.props.room.location}</td>
-                <td></td>
-                </Link>
+        return <tr>
+            <Link to={`/room/${this.props.room._id}`} style={{ all: 'initial' }}>
+                <div style={styleRow} id={this.props.room._id} onMouseOver={this.onMouseOver.bind(this)} onMouseLeave={this.onMouseLeave.bind(this)} onClick={this.onClick.bind(this)}>
+                    <td>{this.props.room.roomName} : </td>
+                    <td><RoomSlot key={this.props.room._id} room={this.props.room} /></td>
+                    <td></td>
+                </div>
+            </Link>
+        </tr>
+
+    }
+}
+
+class RoomSlot extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        var that = this;
+
+        const styleRED = {
+            backgroundColor: 'red',
+            width : '50px',
+            textAlign: 'center',
+        }
+        const styleGREEN = {
+            backgroundColor: 'green',
+            width : '50px',
+            textAlign: 'center',
+        }
+        const styleYELLOW = {
+            backgroundColor: 'yellow',
+            width : '50px',
+            textAlign: 'center',
+        }
+
+        const styleGRAY = {
+            backgroundColor: 'gray',
+            width : '50px',
+            textAlign: 'center',
+        }
+        return <table>
+            <tr>{
+                this.props.room.slots.map(function (element) {
+                    var d = new Date();
+                    let currentHr = d.getHours();
+                    // console.log(`CurrentHr : ${currentHr} !== ${element.slot} and that.props.room.available = ${that.props.room.available}`)
+
+                    if (element.available) {
+                        if (parseInt(element.slot) > currentHr) {
+                            return <td style={styleGREEN}>{element.slot}</td>
+                        }
+                        else if (parseInt(element.slot) == currentHr) {
+                            if(that.props.room.available){
+                                return <td style={styleGREEN}>{element.slot}</td>
+                            }else{
+                                return <td style={styleRED}>{element.slot}</td>
+                            }
+
+                        } else if (parseInt(element.slot) < currentHr) {
+                            // return <td bgcolor="red">{element.slot}</td>
+                            return <td style={styleGRAY}>{element.slot}</td>
+                        }
+                    }
+                    else if (!element.available) {
+                        if (parseInt(element.slot) > currentHr) {
+                            return <td style={styleRED}>{element.slot}</td>
+                        }
+                        else if (parseInt(element.slot) == currentHr) {
+                            if(that.props.room.available){
+                                return <td style={styleYELLOW}>{element.slot}</td>
+                            }else{
+                                return <td style={styleRED}>{element.slot}</td>
+                            }
+
+
+                        } else if (parseInt(element.slot) < currentHr) {
+                            // return <td bgcolor="red">{element.slot}</td>
+                            return <td style={styleGRAY}>{element.slot}</td>
+                        }
+                    }
+
+
+                })
+            }
             </tr>
-        
+        </table>
     }
 }
 
 
-class sample extends React.Component{
+class BookingPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     //https://reacttraining.com/react-router/
-    render(){
-        return <p>
-                    LOADED this
-            </p>
+    render() {
+        console.dir(this.props);
+        return <div>
+            Room name : <input></input>
+        </div>
     }
 }
 
---
+++ dataTest.js
@@ -0,0 +1,132 @@
+export default {
+    "status": {
+        "message": "successful",
+        "code": "200#success"
+    },
+    "data": [
+        {
+            "_id": "59c654ccc3348d012fcc52d3",
+            "roomName": "Room 1",
+            "available": true,
+            "slots": [
+                {
+                    "slot": "8",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slot": "9",
+                    "available": false,
+                    "bookedBy": "John"
+                },
+                {
+                    "slotNum": "10",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "11",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "12",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "13",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "14",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "15",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "16",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "17",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "18",
+                    "available": true,
+                    "bookedBy": null
+                }
+            ]
+        },
+        {
+            "_id": "59c65595c3348d012fcc52d5",
+            "roomName": "Room 1",
+            "available": true,
+            "slots": [
+                {
+                    "slot": "8",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slot": "9",
+                    "available": false,
+                    "bookedBy": "John"
+                },
+                {
+                    "slotNum": "10",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "11",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "12",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "13",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "14",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "15",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "16",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "17",
+                    "available": true,
+                    "bookedBy": null
+                },
+                {
+                    "slotNum": "18",
+                    "available": true,
+                    "bookedBy": null
+                }
+            ]
+        }
+    ]
+}
